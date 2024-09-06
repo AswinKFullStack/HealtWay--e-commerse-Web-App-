f9e20a9ef3e957877419f262697e009d51cb2ba6
Aswin K <aswinkplacement@gmail.com>
2024-09-06 19:54:29 +0530

    Schema setup

---
+++ models/productSchema.js
@@ -0,0 +1,59 @@
+ const mongoose = require("mongoose");
+ const {Schema} = mongoose;
+
+
+ const productSchema = new Schema({
+    productName:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    brand :{
+        type:String,
+        required:true
+    },
+    category:{
+        type:Schema.Types.ObjectId,
+        ref:"Category",
+        required:true
+    },
+    regularPrice:{
+        type:Number,
+        required:true
+    },
+    salePrice:{
+        type:Number,
+        required:true
+    },
+    productOffer:{
+        type:Number,
+        default:0
+    },
+    quantity:{
+        type:Number,
+        default:true
+    },
+    productImage:{
+        type:[String], //multiple images will be there,so that we deifine inside an array
+        required:true
+
+    },
+    isBlocked:{
+        type:Boolean,
+        default:false
+    },
+    status:{
+        type:String,
+        enum:["Available","out of stock ","Discountinued"],
+        required:true,
+        default:"Available"
+    },
+},
+{timestampe:true}
+);
+
+const Product = mongoose.model("Product",productSchema);
+module.exports = Product;
---
+++ models/userSchema.js
@@ -0,0 +1,85 @@
+ const mongoose = require("mongoose");
+ const {Schema} = mongoose;
+
+
+ const userSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    phone:{
+        type:String,
+        required:false, //may be user signup using google account,so that it not mondatory
+        unique:false,
+        spare:true, //for setting uniqe constrans.there is no phonenumber when signup using single signup.so that we set true //
+        default:null
+    },
+    googleId:{
+        type:String,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:false
+    },
+    isBlocked:{
+        type:Boolean,
+        default:false
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    },
+    cart:[
+        {
+            type:Schema.Types.ObjectId,//it is for a referance id  for cart collection
+            ref:"Cart",
+            
+        }
+    ],
+    wallet:[{
+        type:Schema.Types.ObjectId,
+        ref:"Wishlist"
+    }],
+    orderHistory:[{
+        type:Schema.Types.ObjectId,
+        ref:"Order"
+    }],
+    createdOn:{
+        type:Date,
+        default:Date.now,
+
+    },
+    referalCode:{
+        type:String
+    },
+    redeemed:{
+        type:Boolean
+    },
+    redeemUser:[{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }],
+    searchHistory:[{
+        category:{
+            type:Schema.Types.ObjectId,
+            ref:"Category"
+        },
+        brand:{
+            type:String
+        },
+        searchOn:{
+            type:Date,
+            default:Date.now
+        }
+    }]
+ })
+
+
+ const User = mongoose.model("User", userSchema);
+ module.exports = User;
